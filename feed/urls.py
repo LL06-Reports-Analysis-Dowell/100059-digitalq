3d8ecc70265cfa693e9cf93dc0074d1cff386e15
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-09-07 11:34:22 -0400

    quantity available changes to dictionary

--- feed/urls.py
+++ feed/urls.py
@@ -4,7 +4,6 @@ from feed import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    
     path('orders/',views.order_list),
     path('orders/<int:id>', views.order_detail),
 
