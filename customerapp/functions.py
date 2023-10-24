74815e5aec87b5f928db0fa9b0c85f618eb35cf7
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-10-24 14:27:49 -0400

    update order list function added

--- customerapp/functions.py
+++ customerapp/functions.py
@@ -120,3 +120,41 @@ def get_all_order_list():
 
     result = json.loads(response)
     return result
+
+# Update order information
+def update_order(pk, queue, counter, status):
+    
+    url = "http://uxlivinglab.pythonanywhere.com"
+
+    payload = json.dumps({
+       "cluster": "digitalq",
+       "database": "digitalq",
+       "collection": "current_order",
+       "document": "current_order",
+       "team_member_ID": "1140",
+       "function_ID": "ABCDE",
+       "command": "update",
+       "field": {
+              "_id": pk,
+       },
+       "update_field": {
+            "queue": queue,
+            "counter": counter,
+            "status": status
+       },
+       "platform": "bangalore"
+    })
+
+    headers = {
+       'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    # print(response.json())
+    response = response.json()
+
+    result = json.loads(response)
+    return result
+
+
