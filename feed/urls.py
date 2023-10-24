9c4bd5b2e4fad3a6b07cc61e2009c1750df3a481
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-10-24 15:06:28 -0400

    doc updated and menu list api reformatted

--- feed/urls.py
+++ feed/urls.py
@@ -8,21 +8,21 @@ urlpatterns = [
     path('orders/<int:id>', views.order_detail),
 
     ## dish list 
-    path('api/targeted_population/', views.targeted_populations),
-    path('api/post_population/', views.post_populations), # create new dish
-    path('api/update_population/<str:pk>/', views.update_dish), # update dish info
+    path('targeted_population/', views.targeted_populations),
+    path('post_population/', views.post_populations), # create new dish
+    path('update_population/<str:pk>/', views.update_dish), # update dish info
     # retrive dish data
-    path('api/population/', views.population), # retrive all dish
-    path('api/population/eventid/', views.get_eventid_wise_dish), # retrive eventid_wise single dish
-    path('api/population/type/', views.get_same_type_dish_order), # Retrieve Dish list by dish type
-    path('api/population/dish_code/', views.get_dish_code_type_dish_order), # Retrieve Dish list by dish code
-    path('api/population/dish_id/', views.get_id_wise_dish), # Retrieve id wise dish
+    path('population/', views.population), # retrive all dish
+    path('population/eventid/', views.get_eventid_wise_dish), # retrive eventid_wise single dish
+    path('population/type/', views.get_same_type_dish_order), # Retrieve Dish list by dish type
+    path('population/dish_code/', views.get_dish_code_type_dish_order), # Retrieve Dish list by dish code
+    path('population/dish_id/', views.get_id_wise_dish), # Retrieve id wise dish
 
-    path('api/payload/', views.payload),
-    path('api/payload/<int:number>', views.payload_param),
-    path('api/connection/', views.connection),
+    path('payload/', views.payload),
+    path('payload/<int:number>', views.payload_param),
+    path('connection/', views.connection),
     
-    path('api/create_event/', views.create_events), # for testing 
+    path('create_event/', views.create_events), # for testing 
 
 ]
 
