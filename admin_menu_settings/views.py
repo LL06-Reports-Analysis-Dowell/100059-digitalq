15f4825af0b234d842b95c86d26e2f222c004220
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-10-31 02:41:43 -0400

    functions added to the admin menu settings app

--- admin_menu_settings/views.py
+++ admin_menu_settings/views.py
@@ -1,3 +1,16 @@
+import imp
+import json
+import requests
+import pprint
+from pickle import FROZENSET
+from re import T
+import re
+from urllib import response
 from django.shortcuts import render
+from .functions import *
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
